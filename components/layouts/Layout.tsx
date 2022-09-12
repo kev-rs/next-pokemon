2a8ea9c36cdbc5211902bab5392586528ee3339d
kev <kevbs.jsx@gmail.com>
2022-09-12 19:04:22 -0400

    practice-v1

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import React from 'react'
+
+export const Layout = () => {
+  return (
+    <>
+        <Head>
+            
+        </Head>
+        
+    </>
+  )
+}
