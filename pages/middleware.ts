31b427934620e653b8d9e6c11686c954edf97637
kev <kevbs.jsx@gmail.com>
2022-11-09 20:33:46 -0500

    middleware

---
+++ pages/middleware.ts
@@ -0,0 +1,10 @@
+import { type NextRequest, NextResponse } from "next/server";
+
+
+export async function middleware(req: NextRequest) {
+  return NextResponse.rewrite(new URL('/', req.url));
+}
+
+export const config = {
+  matcher: ['/api/:path*']
+}
